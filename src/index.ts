eab15c78a7bf9d03a72bec415d27f821947f0bdc
Bartosz Sadowski <bartek_sadowski@o2.pl>
2023-09-24 13:11:31 +0200

    Configure simple web server and hot reload

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import { AdventureProvider } from './adventure/adventure.provider';
+import express from 'express';
 
 const provider = new AdventureProvider();
-console.log(provider.generate());
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  const adventure = provider.generate();
+  console.log('New adventure generated');
+  console.log(adventure);
+
+  res.send(adventure);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
