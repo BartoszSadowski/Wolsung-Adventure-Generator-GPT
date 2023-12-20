d3d644482d2c78122cab07af1cc43bab8a169c09
Bartosz Sadowski <bartek_sadowski@o2.pl>
2023-12-20 13:06:43 +0100

    Add adventure description

--- src/adventure/view/language/act/advantage/gpt.advantage.provider.ts
+++ src/adventure/view/language/act/advantage/gpt.advantage.provider.ts
@@ -1,10 +1,25 @@
 import { AdvantageProvider } from './advantage.provider';
 import { Scene } from '../../../../scene/scene';
 import { AdvantageView } from './advantage.view';
+import { GptProvider } from '../../gpt/gpt.provider';
 
 export class GptAdvantageProvider implements AdvantageProvider {
   async get(scene: Scene): Promise<Array<AdvantageView>> {
-    // TODO replace me with actual call
-    return [];
+    return Promise.all(scene.advantages.map(async ({ traits, ...traitlessAdvantage }) => {
+      const description = await this.getDescription(traitlessAdvantage);
+
+      return new AdvantageView(
+        description,
+        traits,
+      );
+    }));
+  }
+
+  private async getDescription(traitlessAdvantage: Record<string, unknown>): Promise<string> {
+    const advantageText = JSON.stringify(traitlessAdvantage);
+
+    return GptProvider.getDescriptionGpt()
+      .get(`Napisz akapit opisujący element świata przedstawionego do użycia w przygododzie przez mistrza gry
+       na podstawie następujących danych: ${advantageText}`);
   }
 }
